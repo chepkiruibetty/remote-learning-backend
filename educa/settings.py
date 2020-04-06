e134bdbd19bf103c8dc54f360361bf7fe898c3e9
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-06 12:34:06 +0300

    created templates for different folders

--- educa/settings.py
+++ educa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,7 +37,9 @@ CORS_ORIGIN_WHITELIST = (
 INSTALLED_APPS = [
     'django.contrib.admin',
     'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
     'corsheaders',
+    'embed_video',
     'rest_framework',
     'rest_auth',
     'rest_framework.authtoken', 
@@ -144,8 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# LOGIN_REDIRECT_URL='home'
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 # LOGOUT_REDIRECT_URL='home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID =1
 CORS_ORIGIN_ALLOW_ALL = True
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- educa/urls.py
+++ educa/urls.py
@@ -16,14 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('course/', include('courses.urls')),
+    path('students/', include('students.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
     path('api/', include('courses.api.urls',namespace='api')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', # new
     include('rest_auth.registration.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+               document_root=settings.MEDIA_ROOT)
