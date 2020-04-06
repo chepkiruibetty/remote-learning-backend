d397ade73c2b3955ce6f43dddf1d923b602ff9a5
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-04-06 12:33:01 +0300

    add UserViewset

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path,include
 from rest_framework import routers
 from . import views
+from .views import UserList, UserDetail,UserViewSet
+
+
 app_name = 'courses'
 router = routers.DefaultRouter()
 router.register('courses', views.CourseViewSet,basename='Course')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
 path('subjects/',
@@ -14,6 +18,10 @@ path('subjects/<pk>/',
     views.SubjectDetailView.as_view(),
     name='subject_detail'),
 
+path('users/', UserList.as_view()), 
+path('users/<int:pk>/', UserDetail.as_view()),
+
 
 path('', include(router.urls)),
+
 ]
